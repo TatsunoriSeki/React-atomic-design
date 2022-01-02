319e4f2bdcdd63cbf9a3daa6094405a835665e8c
TatsunoriSeki <51009619+TatsunoriSeki@users.noreply.github.com>
2022-01-03 03:28:41 +0900

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,6 @@
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  name: "TestName",
-  mail: "example@example.com",
-  tel: "111-2222-3333",
-  company: {
-    name: "TestCompany"
-  },
-  WEB: "https://google.com"
-};
-
 export default function App() {
-  return (
-    <div className="App">
-      <PrimaryButton>Test</PrimaryButton>
-      <SecondaryButton>Search</SecondaryButton>
-      <br />
-      <SearchInput />
-      <UserCard user={user} />
-    </div>
-  );
+  return <Router />;
 }
---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    image: "https://source.unsplash.com/NE0XGVKTmcA",
+    name: "TestName",
+    mail: "example@example.com",
+    tel: "111-2222-3333",
+    company: {
+      name: "TestCompany"
+    },
+    WEB: "https://google.com"
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>Users Page</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard keys={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmaxx(200px, 1fr));
+  grid-gap: 20px;
+`;
